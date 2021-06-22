14670ab49bed3bf7dfdc632f58a0309e4132e72e
dxadly <adlydavid2@gmail.com>
2021-06-22 08:46:28 -0700

    df

---
+++ script.js
@@ -0,0 +1,8 @@
+import * as THREE from "https://cdn.skypack.dev/three";
+
+import { GLTFLoader } from "https://cdn.skypack.dev/three/examples/jsm/loaders/GLTFLoader.js";
+import { DRACOLoader } from "https://cdn.skypack.dev/three/examples/jsm/loaders/DRACOLoader.js";
+
+import { OrbitControls } from "https://cdn.skypack.dev/three/examples/jsm/controls/OrbitControls.js";
+
+import store from "./store.js";
