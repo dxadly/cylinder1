1ab39170b0bcb6940fbd3b1f1f17598362e987d1
dxadly <adlydavid2@gmail.com>
2021-06-24 06:16:15 -0700

    sadf

--- script.js
+++ script.js
@@ -1,102 +1,67 @@
-import * as THREE from "https://cdn.skypack.dev/three@0.129?min"
-import { OrbitControls } from "https://cdn.skypack.dev/three@0.129.0/examples/jsm/controls/OrbitControls?min"
-import { EffectComposer, Pass, FullScreenQuad } from "https://cdn.skypack.dev/three@0.129.0/examples/jsm/postprocessing/EffectComposer?min"
-import { RenderPass } from 'https://cdn.skypack.dev/three@0.129.0/examples/jsm/postprocessing/RenderPass?min'
-import gsap from 'https://cdn.skypack.dev/gsap';
-import { CSGModeller } from 'https://cdn.jsdelivr.net/gh/ycw/three-csg-modeller@0.1.10/src/index.js'
-import { DualBloomPassGen } from 'https://cdn.jsdelivr.net/gh/ycw/three-dual-bloom@1.1.7/src/index.js'
+import * as THREE from "https://cdn.skypack.dev/three";
 
-// ----
-// main ( @2021-05-31 )
-// ----
+import { GLTFLoader } from "https://cdn.skypack.dev/three/examples/jsm/loaders/GLTFLoader.js";
+import { DRACOLoader } from "https://cdn.skypack.dev/three/examples/jsm/loaders/DRACOLoader.js";
 
-const renderer = new THREE.WebGLRenderer();
-const scene = new THREE.Scene();
-const camera = new THREE.PerspectiveCamera(75, 2, .1, 100);
-const controls = new OrbitControls(camera, renderer.domElement);
+import { OrbitControls } from "https://cdn.skypack.dev/three/examples/jsm/controls/OrbitControls.js";
 
-camera.position.set(1, 2, 7);
-controls.enableDamping = true;
+import store from "./store.js";
+var scene = new THREE.Scene();
+// setup the camera
+/*
+PerspectiveCamera( fov, aspect, near, far )
 
-const light = new THREE.PointLight('tomato', 1);
-scene.add(light);
+fov — Camera frustum vertical field of view.
+aspect — Camera frustum aspect ratio.
+near — Camera frustum near plane.
+far — Camera frustum far plane.
+*/
+var camera = new THREE.PerspectiveCamera( 30 /* zoom in/out  */, window.innerWidth/window.innerHeight, 0.1, 1000 );
 
-// ----
-// csg mod
-// ----
+/*
+WebGLRenderer( parameters )
 
-const modeller = new CSGModeller(THREE);
-const model0 = modeller.model(new THREE.Mesh(
-  new THREE.BoxBufferGeometry(),
-  new THREE.MeshPhongMaterial()
-));
-const model1 = model0.applyMatrix4(new THREE.Matrix4().makeScale(.7, .7, 2));
-const model2 = model0.applyMatrix4(new THREE.Matrix4().makeScale(.7, 2, .7));
-const model3 = model0.applyMatrix4(new THREE.Matrix4().makeScale(2, .7, .7));
-const mesh = model0.subtract(model1).subtract(model2).subtract(model3).build();
+parameters is an optional object with properties defining the renderer's behaviour. The constructor also accepts no parameters at all. In all cases, it will assume sane defaults when parameters are missing.
+canvas — A Canvas where the renderer draws its output.
+precision — shader precision. Can be "highp", "mediump" or "lowp".
+alpha — Boolean, default is false.
+premultipliedAlpha — Boolean, default is true.
+antialias — Boolean, default is false.
+stencil — Boolean, default is true.
+preserveDrawingBuffer — Boolean, default is false.
+maxLights — Integer, default is 4.
+*/
+var renderer = new THREE.WebGLRenderer();
 
-// ----
-// array mod
-// ----
+renderer.setSize( window.innerWidth, window.innerHeight );
+document.body.appendChild( renderer.domElement );
 
-const rSeg = 20;
-const hSeg = 40;
-const geom = new THREE.CylinderBufferGeometry(6, 6, 100, rSeg, hSeg, true, 0, Math.PI * 2 * (rSeg - 1) / rSeg);
-const nInst = geom.attributes.position.count;
 
-const inst = new THREE.InstancedMesh(mesh.geometry, mesh.material, nInst);
-const instMatrix = new THREE.Matrix4();
-for (let i = 0, I = nInst; i < I; ++i) {
-  const n = (i % rSeg) / rSeg * 5;
-  instMatrix.setPosition(
-    geom.attributes.position.getX(i),
-    geom.attributes.position.getY(i),
-    geom.attributes.position.getZ(i),
-  ).multiply(new THREE.Matrix4().makeRotationZ(n * Math.PI));
-  inst.setMatrixAt(i, instMatrix);
-  inst.setColorAt(i, new THREE.Color().setHSL(n, .5, .5));
-}
-inst.instanceMatrix.needsUpdate = true;
-inst.instanceColor.needsUpdate = true;
-scene.add(inst);
-geom.dispose();
+/*
+BoxGeometry(width, height, depth, widthSegments, heightSegments, depthSegments)
 
-// ----
-// render
-// ----
+width — Width of the sides on the X axis.
+height — Height of the sides on the Y axis.
+depth — Depth of the sides on the Z axis.
+widthSegments — Optional. Number of segmented faces along the width of the sides. Default is 1.
+heightSegments — Optional. Number of segmented faces along the height of the sides. Default is 1.
+depthSegments — Optional. Number of segmented faces along the depth of the sides. Default is 1.
+*/
+var geometry = new THREE.BoxGeometry( 1, 1, 1 );
 
-const DualBloomPass = new DualBloomPassGen({ THREE, Pass, FullScreenQuad });
-const dualBloomPass = new DualBloomPass({
-  threshold: .8,
-  blurriness: .2,
-  intensity: 5,
-});
+var material = new THREE.MeshBasicMaterial( { color: 0xaa0000 } );
+var cube = new THREE.Mesh( geometry, material );
+scene.add( cube );
 
-const composer = new EffectComposer(renderer);
-composer.addPass(new RenderPass(scene, camera));
-composer.addPass(dualBloomPass);
+camera.position.z = 10;
 
-renderer.setAnimationLoop(() => {
-  composer.render();
-  controls.update();
-});
+var render = function () {
+  requestAnimationFrame( render );
 
-gsap.to(dualBloomPass, { threshold: .1, intensity: 2, duration: 5 });
-gsap.to(inst.position, { y: 10, duration: 5, repeat: -1, ease: 'none' });
-gsap.to(inst.rotation, { y: Math.PI * 2, duration: 5 * 12, repeat: -1, ease: 'none' });
+  cube.rotation.x += 0.01;
+  cube.rotation.y += 0.01;
 
-// ----
-// view
-// ----
+  renderer.render(scene, camera);
+};
 
-function resize(w, h, dpr = devicePixelRatio) {
-  renderer.setPixelRatio(dpr);
-  renderer.setSize(w, h, false);
-  composer.setPixelRatio(dpr);
-  composer.setSize(w, h);
-  camera.aspect = w / h;
-  camera.updateProjectionMatrix();
-}
-addEventListener('resize', () => resize(innerWidth, innerHeight));
-dispatchEvent(new Event('resize'));
-document.body.prepend(renderer.domElement);
+render();
