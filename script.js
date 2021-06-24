3e614ca58617a47b9f18c9a9fd2d7605d2ea6e3f
dxadly <adlydavid2@gmail.com>
2021-06-24 06:06:00 -0700

    dasdf

--- script.js
+++ script.js
@@ -1,70 +1,102 @@
-import * as THREE from "https://cdn.skypack.dev/three";
+import * as THREE from '//cdn.skypack.dev/three@0.129?min'
+import { OrbitControls } from '//cdn.skypack.dev/three@0.129.0/examples/jsm/controls/OrbitControls?min'
+import { EffectComposer, Pass, FullScreenQuad } from '//cdn.skypack.dev/three@0.129.0/examples/jsm/postprocessing/EffectComposer?min'
+import { RenderPass } from '//cdn.skypack.dev/three@0.129.0/examples/jsm/postprocessing/RenderPass?min'
+import gsap from '//cdn.skypack.dev/gsap';
+import { CSGModeller } from '//cdn.jsdelivr.net/gh/ycw/three-csg-modeller@0.1.10/src/index.js'
+import { DualBloomPassGen } from '//cdn.jsdelivr.net/gh/ycw/three-dual-bloom@1.1.7/src/index.js'
 
-import { GLTFLoader } from "https://cdn.skypack.dev/three/examples/jsm/loaders/GLTFLoader.js";
-import { DRACOLoader } from "https://cdn.skypack.dev/three/examples/jsm/loaders/DRACOLoader.js";
+// ----
+// main ( @2021-05-31 )
+// ----
 
-import { OrbitControls } from "https://cdn.skypack.dev/three/examples/jsm/controls/OrbitControls.js";
+const renderer = new THREE.WebGLRenderer();
+const scene = new THREE.Scene();
+const camera = new THREE.PerspectiveCamera(75, 2, .1, 100);
+const controls = new OrbitControls(camera, renderer.domElement);
 
-import store from "./store.js";
+camera.position.set(1, 2, 7);
+controls.enableDamping = true;
 
-// 准备场景、相机、渲染器、光线
-let scene = null;
-let camera = null;
-let renderer = null;
-let light = null;
-let initThree =  function() {
-    // 创建渲染器
-    const canvas = document.querySelector('#three_canvas');
-    renderer = new THREE.WebGLRenderer({canvas});// 创建渲染器，canvas可传可不传。传一个canvas对象更灵活
-    renderer.setSize(window.innerWidth, window.innerHeight);// 设置渲染器的宽高
+const light = new THREE.PointLight('tomato', 1);
+scene.add(light);
 
-    // 创建相机
-    const fov = 75;// 视野范围，垂直方向75度角
-    const aspect = window.innerWidth / window.innerHeight;// 画布的宽高比，默认是2，建议使用画布在浏览器里的大小比，
-    const near = 0.1;// 近平面
-    const far = 1000;// 远平面，近平面和远平面限制了摄像机面朝方向的可绘区域，任何距离小于或超过这个范围的物体都将被裁剪掉(不绘制)
-    camera = new THREE.PerspectiveCamera(fov, aspect, near, far);
-    camera.position.z = 10;
+// ----
+// csg mod
+// ----
 
-    // 创建场景
-    scene = new THREE.Scene();
+const modeller = new CSGModeller(THREE);
+const model0 = modeller.model(new THREE.Mesh(
+  new THREE.BoxBufferGeometry(),
+  new THREE.MeshPhongMaterial()
+));
+const model1 = model0.applyMatrix4(new THREE.Matrix4().makeScale(.7, .7, 2));
+const model2 = model0.applyMatrix4(new THREE.Matrix4().makeScale(.7, 2, .7));
+const model3 = model0.applyMatrix4(new THREE.Matrix4().makeScale(2, .7, .7));
+const mesh = model0.subtract(model1).subtract(model2).subtract(model3).build();
 
-    // 创建一束光线
-    const color = 0xFFFFFF;// 光颜色
-    const intensity = 2;// 光照强度
-    light = new THREE.DirectionalLight(color, intensity);
-    light.position.set(0, 0, 10);// 设置光源发射点的位置
-    scene.add(light);
+// ----
+// array mod
+// ----
+
+const rSeg = 20;
+const hSeg = 40;
+const geom = new THREE.CylinderBufferGeometry(6, 6, 100, rSeg, hSeg, true, 0, Math.PI * 2 * (rSeg - 1) / rSeg);
+const nInst = geom.attributes.position.count;
+
+const inst = new THREE.InstancedMesh(mesh.geometry, mesh.material, nInst);
+const instMatrix = new THREE.Matrix4();
+for (let i = 0, I = nInst; i < I; ++i) {
+  const n = (i % rSeg) / rSeg * 5;
+  instMatrix.setPosition(
+    geom.attributes.position.getX(i),
+    geom.attributes.position.getY(i),
+    geom.attributes.position.getZ(i),
+  ).multiply(new THREE.Matrix4().makeRotationZ(n * Math.PI));
+  inst.setMatrixAt(i, instMatrix);
+  inst.setColorAt(i, new THREE.Color().setHSL(n, .5, .5));
 }
+inst.instanceMatrix.needsUpdate = true;
+inst.instanceColor.needsUpdate = true;
+scene.add(inst);
+geom.dispose();
 
-// 初始化球体
-let sunMesh = null;
-let initBox = function() {
-    const radius = 1;
-    const widthSegments = 32;
-    const heightSegments = 32;
-    const geometry = new THREE.SphereGeometry(radius, widthSegments, heightSegments);
+// ----
+// render
+// ----
 
-    // 太阳
-    const loader = new THREE.TextureLoader();
-    const texture = loader.load('https://webjunjun.github.io/wechat/earth.jpg');
-    texture.offset.y = 0.1;// 偏移贴图
-    const sunMaterial = new THREE.MeshPhongMaterial({
-        map: texture,
-    });
-    sunMesh = new THREE.Mesh(geometry, sunMaterial);
-    sunMesh.scale.set(4, 4, 4);
-    scene.add(sunMesh);
+const DualBloomPass = new DualBloomPassGen({ THREE, Pass, FullScreenQuad });
+const dualBloomPass = new DualBloomPass({
+  threshold: .8,
+  blurriness: .2,
+  intensity: 5,
+});
+
+const composer = new EffectComposer(renderer);
+composer.addPass(new RenderPass(scene, camera));
+composer.addPass(dualBloomPass);
+
+renderer.setAnimationLoop(() => {
+  composer.render();
+  controls.update();
+});
+
+gsap.to(dualBloomPass, { threshold: .1, intensity: 2, duration: 5 });
+gsap.to(inst.position, { y: 10, duration: 5, repeat: -1, ease: 'none' });
+gsap.to(inst.rotation, { y: Math.PI * 2, duration: 5 * 12, repeat: -1, ease: 'none' });
+
+// ----
+// view
+// ----
+
+function resize(w, h, dpr = devicePixelRatio) {
+  renderer.setPixelRatio(dpr);
+  renderer.setSize(w, h, false);
+  composer.setPixelRatio(dpr);
+  composer.setSize(w, h);
+  camera.aspect = w / h;
+  camera.updateProjectionMatrix();
 }
-
-function animate(time) {
-    time *= 0.0002;
-    sunMesh.rotation.y = time;
-    renderer.render(scene, camera);
-    requestAnimationFrame(animate);
-}
-
-// 启动方法或其他初始化方法
-initThree();
-initBox();
-animate();
+addEventListener('resize', () => resize(innerWidth, innerHeight));
+dispatchEvent(new Event('resize'));
+document.body.prepend(renderer.domElement);
