08dfbc081104d2b19afc2bd15713407bb1a173f5
dxadly <adlydavid2@gmail.com>
2021-06-24 06:02:45 -0700

    ds

--- script.js
+++ script.js
@@ -7,137 +7,64 @@ import { OrbitControls } from "https://cdn.skypack.dev/three/examples/jsm/contro
 
 import store from "./store.js";
 
-import * as THREE from '//cdn.skypack.dev/three@0.129.0?min'
-import { OrbitControls } from '//cdn.skypack.dev/three@0.129.0/examples/jsm/controls/OrbitControls?min'
-import webfontloader from '//cdn.skypack.dev/webfontloader@1.6.28?min'
+// 准备场景、相机、渲染器、光线
+let scene = null;
+let camera = null;
+let renderer = null;
+let light = null;
+let initThree =  function() {
+    // 创建渲染器
+    const canvas = document.querySelector('#three_canvas');
+    renderer = new THREE.WebGLRenderer({canvas});// 创建渲染器，canvas可传可不传。传一个canvas对象更灵活
+    renderer.setSize(window.innerWidth, window.innerHeight);// 设置渲染器的宽高
 
-// ----
-// tex
-// ----
+    // 创建相机
+    const fov = 75;// 视野范围，垂直方向75度角
+    const aspect = window.innerWidth / window.innerHeight;// 画布的宽高比，默认是2，建议使用画布在浏览器里的大小比，
+    const near = 0.1;// 近平面
+    const far = 1000;// 远平面，近平面和远平面限制了摄像机面朝方向的可绘区域，任何距离小于或超过这个范围的物体都将被裁剪掉(不绘制)
+    camera = new THREE.PerspectiveCamera(fov, aspect, near, far);
+    camera.position.z = 10;
 
-const can = document.createElement('canvas');
-const tex0 = new THREE.CanvasTexture(can, THREE.UVMapping, THREE.RepeatWrapping);
-const fontSize = 256;
-const font = `bold ${fontSize}px "Viaoda Libre"`;
-const text = `  wraparound  `.repeat(2) // the msg
+    // 创建场景
+    scene = new THREE.Scene();
 
-webfontloader.load({
-  google: {
-    families: ['Viaoda Libre']
-  },
-  active: makeTex,
-  inactive: () => alert('failed to load font, bye.')
-});
-
-function makeTex() {
-  const ctx = can.getContext('2d');
-  ctx.font = font;
-
-  can.width = ctx.measureText(text).width;
-  can.height = fontSize * 4 / 3;
-
-  ctx.font = font;
-  ctx.fillStyle = 'lime';
-  ctx.textAlign = 'left';
-  ctx.textBaseline = 'middle';
-  ctx.fillText(text, 0, can.height / 2);
-
-  tex0.needsUpdate = true;
+    // 创建一束光线
+    const color = 0xFFFFFF;// 光颜色
+    const intensity = 2;// 光照强度
+    light = new THREE.DirectionalLight(color, intensity);
+    light.position.set(0, 0, 10);// 设置光源发射点的位置
+    scene.add(light);
 }
 
-// ----
-// main
-// ----
+// 初始化球体
+let sunMesh = null;
+let initBox = function() {
+    const radius = 1;
+    const widthSegments = 32;
+    const heightSegments = 32;
+    const geometry = new THREE.SphereGeometry(radius, widthSegments, heightSegments);
 
-const renderer = new THREE.WebGLRenderer();
-const scene = new THREE.Scene();
-const camera = new THREE.PerspectiveCamera(75, 2, .1, 100);
-const controls = new OrbitControls(camera, renderer.domElement);
-
-scene.background = new THREE.Color('#ddd');
-camera.position.set(0, 2, 12);
-controls.enableDamping = true;
-renderer.shadowMap.enabled = true;
-
-const light = new THREE.DirectionalLight('white', .5);
-light.castShadow = true;
-light.position.set(0, 5, 5);
-light.shadow.camera.far = 32;
-light.shadow.camera.bottom = -20;
-light.shadow.mapSize.setScalar(1024);
-scene.add(light);
-
-scene.add(new THREE.AmbientLight('white', .5));
-
-// ----
-// prod model - stub
-// ----
-
-{
-  const geom = new THREE.CylinderGeometry(2, 2, 15, 50);
-  const mat = new THREE.MeshPhongMaterial({});
-  const mesh = new THREE.Mesh(geom, mat);
-  mesh.receiveShadow = true;
-  mesh.castShadow = true;
-  scene.add(mesh);
+    // 太阳
+    const loader = new THREE.TextureLoader();
+    const texture = loader.load('https://webjunjun.github.io/wechat/earth.jpg');
+    texture.offset.y = 0.1;// 偏移贴图
+    const sunMaterial = new THREE.MeshPhongMaterial({
+        map: texture,
+    });
+    sunMesh = new THREE.Mesh(geometry, sunMaterial);
+    sunMesh.scale.set(4, 4, 4);
+    scene.add(sunMesh);
 }
 
-// ----
-// ribbon
-// ----
-
-const ribbon = (() => {
-  const W = 15; // width
-  const H = 4; // height
-  const F = 3; // n turns
-  const R = 2 + 1; // radius
-  const tex1 = new THREE.TextureLoader().load('https://images.unsplash.com/photo-1469980098053-382eb10ba017?ixid=MnwxMjA3fDB8MHxzZWFyY2h8OTl8fHN0YXJ8ZW58MHx8MHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=600&q=60');
-
-  const geom = new THREE.ParametricGeometry((u, v, dst) => {
-    const a = (u * F * 2 + 1) * Math.PI; // twist 90d to hide seam
-    const x = R * Math.sin(a);
-    const z = R * Math.cos(a);
-    const y = (0.5 - u) * W + (v - 0.5) * H;
-    dst.set(x, y, z);
-  }, 100, 1);
-  const mat = new THREE.MeshPhongMaterial({
-    map: tex1, // tint
-    side: THREE.DoubleSide,
-    alphaMap: tex0,
-    alphaTest: 0.5,
-  });
-  const mesh = new THREE.Mesh(geom, mat);
-  mesh.castShadow = true;
-  mesh.customDepthMaterial = new THREE.MeshDepthMaterial({
-    depthPacking: THREE.RGBADepthPacking,
-    alphaMap: mat.alphaMap,
-    alphaTest: mat.alphaTest
-  });
-  return mesh;
-})();
-scene.add(ribbon);
-
-// ----
-// render
-// ----
-
-renderer.setAnimationLoop(() => {
-  renderer.render(scene, camera);
-  controls.update();
-  tex0.offset.x += 0.001;
-  ribbon.material.map.offset.x += 0.001;
-});
-
-// ----
-// view
-// ----
-
-function resize(w, h, dpr = devicePixelRatio) {
-  renderer.setPixelRatio(dpr);
-  renderer.setSize(w, h, false);
-  camera.aspect = w / h;
-  camera.updateProjectionMatrix();
+function animate(time) {
+    time *= 0.0002;
+    sunMesh.rotation.y = time;
+    renderer.render(scene, camera);
+    requestAnimationFrame(animate);
 }
-addEventListener('resize', () => resize(innerWidth, innerHeight));
-dispatchEvent(new Event('resize'));
-document.body.prepend(renderer.domElement);
+
+// 启动方法或其他初始化方法
+initThree();
+initBox();
+animate();
